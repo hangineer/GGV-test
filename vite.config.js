0e0e986d57a3316b452d37513aea018a169eebd1
王涵 <hannahwang@locahost.localdomain>
2023-07-18 00:33:49 +0800

    feat: create folder stracture

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/GGV-test/',
+  base: '/hex-layout-class/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
